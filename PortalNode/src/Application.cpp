ac0fc76d72e63dcf52887f64233de3696ff6f2d6
Nikolaus Karpinsky <karpinsn@gmail.com>
2012-12-17 22:00:48 -0600

    Got the code compiling and displaying a blank window

--- PortalNode/src/Application.cpp
+++ PortalNode/src/Application.cpp
@@ -5,6 +5,12 @@ int main(int argc, char **argv)
 	//	Set the logger level
 	wrench::Logger::setLogLevel(wrench::LogLevel::Error);
 
+	//	Initalize a context and give it to the window manager
+	PortalGLContext context;
+	GLUTManager manager(&context, argc, argv);
+
 	// Start up the application run loop
+	manager.start();
+
 	return 0; 
 }
---
+++ PortalNode/src/PortalGLContext.cpp
@@ -0,0 +1,30 @@
+#include "PortalGLContext.h"
+
+void PortalGLContext::init(void)
+{
+}
+
+void PortalGLContext::init(int width, int height)
+{
+}
+
+void PortalGLContext::draw(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void PortalGLContext::resize(int width, int height)
+{
+}
+
+void PortalGLContext::cameraSelectMode(int mode)
+{
+}
+
+void PortalGLContext::mousePressEvent(int mouseX, int mouseY)
+{
+}
+
+void PortalGLContext::mouseMoveEvent(int mouseX, int mouseY)
+{
+}
