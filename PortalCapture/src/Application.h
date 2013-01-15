a56754468f56f9bed6332645bb808456dfd4fe83
Nik Karpinsky <karpinsn@iastate.edu>
2013-01-15 11:35:08 -0600

    Initial add of the portal capture subsystem

---
+++ PortalCapture/src/Application.h
@@ -0,0 +1,11 @@
+/**
+ @file
+ @author		Nikolaus Karpinsky
+ */
+
+#ifndef _PORTAL_NODE_APPLICATION_H_
+#define _PORTAL_NODE_APPLICATION_H_
+
+#include "wrench\Logger.h"
+
+#endif	// _PORTAL_NODE_APPLICATION_H_
