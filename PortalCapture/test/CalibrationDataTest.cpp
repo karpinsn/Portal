fd3196d4cf475eecaa1e901092a0d24241188cbb
Nikolaus Karpinsky <karpinsn@gmail.com>
2013-04-14 16:25:49 -0500

    Got the project to build and compile with tests. Now need to work on linking in project sources

---
+++ PortalCapture/test/CalibrationDataTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+//#include "CalibrationData.h"
+
+TEST(CalibrationData, GenerateFringe_CorrectN)
+{
+  //NFringeStructuredLight structuredLight(5);
+  //auto fringe = structuredLight.GenerateFringe(cv::Size(128,128), 70, IStructuredLight::Horizontal);
+
+  EXPECT_EQ(5, 5);
+}
